84e0e032b20d93cf0874a483bca9601e2a34d8ad
Sebastian <sebastiankozlowski84@gmail.com>
2022-11-03 21:01:51 +0100

    Small dataclass tests

--- src/github_user/user_info.py
+++ src/github_user/user_info.py
@@ -1,2 +1,14 @@
-def some_func() -> str:
-    return "test-str"
+from dataclasses import dataclass
+from dataclasses import asdict
+
+
+@dataclass
+class DataToPharse(object):
+
+    data: str
+
+
+@dataclass
+class OutputData:
+
+    my_list = list[DataToPharse]
--- tests/test_user_info.py
+++ tests/test_user_info.py
@@ -1,6 +1,24 @@
-import pytest
-from github_user.user_info import some_func
+from github_user.user_info import DataToPharse
+from dataclasses import asdict
 
 
-def test_some_func():
-    assert some_func() == "test-str"
+class TestDataToPharse:
+    def test_empty_list(self):
+        data = DataToPharse([])
+        assert data.data == []
+
+    def test_str_object(self):
+        data = DataToPharse("test str")
+        assert data.data == "test str"
+
+    def test_one_item_in_list(self):
+        data = DataToPharse([1])
+        assert data.data == [1]
+
+    def test_five_mix_different_in_list(self):
+        data = DataToPharse(["one", 2, [3, 3], {5: 1}, (0, 0)])
+        assert data.data == ["one", 2, [3, 3], {5: 1}, (0, 0)]
+
+    def test_astict_dict(self):
+        sample_dict = DataToPharse("example_key")
+        assert asdict(sample_dict) == {"data": "example_key"}
