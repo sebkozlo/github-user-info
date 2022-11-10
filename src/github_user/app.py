0c545576abc0c6b9cb2a382c200e7f1c157a003c
sebkozlo <sebastiankozlowski84@gmail.com>
2022-11-10 16:30:05 +0100

    Creating a simple frontend section for the app

--- src/github_user/app.py
+++ src/github_user/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello(name=None):
     return render_template('index.html', name=name)
 
+
+@app.route("/form", methods=["POST"])
+def get_user_nick():
+    user_nick = request.form.get("search_user")
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
